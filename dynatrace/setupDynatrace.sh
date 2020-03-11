07a9ea09c20392402186d461093ac1fbedffbdf2
jetzlstorfer <juergen.etzlstorfer@dynatrace.com>
2020-03-11 08:53:13 -0400

    delete secret before creation

--- dynatrace/setupDynatrace.sh
+++ dynatrace/setupDynatrace.sh
@@ -7,14 +7,12 @@ DT_PAAS_TOKEN=$(cat creds_dt.json | jq -r '.dynatracePaasToken')
 echo "============================================================="
 echo "creating Dynatrace secret"
 echo "============================================================="
-
+kubectl delete secret dynatrace -n keptn 2> /dev/null
 kubectl -n keptn create secret generic dynatrace --from-literal="DT_TENANT=$DT_TENANT" --from-literal="DT_API_TOKEN=$DT_API_TOKEN"  --from-literal="DT_PAAS_TOKEN=$DT_PAAS_TOKEN"
 
 echo "============================================================="
 echo "deploying Dynatrace service"
 echo "============================================================="
-
-
 kubectl apply -f https://raw.githubusercontent.com/keptn-contrib/dynatrace-service/0.6.1/deploy/manifests/dynatrace-service/dynatrace-service.yaml
 
 echo "============================================================="
@@ -25,7 +23,6 @@ keptn configure monitoring dynatrace
 echo "============================================================="
 echo "waiting 90 seconds for Dynatrace OneAgent to be deployed"
 echo "============================================================="
-
 sleep 90
 kubectl get pods -n dynatrace
 
@@ -33,7 +30,6 @@ kubectl get pods -n dynatrace
 echo "============================================================="
 echo "deploying Dynatrace SLI service"
 echo "============================================================="
-
 kubectl apply -f https://raw.githubusercontent.com/keptn-contrib/dynatrace-sli-service/0.3.0/deploy/service.yaml
 
 echo "============================================================="
